165b644d4458ac7318fcb15a553a3e9fa5833516
eccentric58 <cool.eccentric@yandex.ru>
2025-01-22 11:33:49 +0300

    upd: Data

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/model/Stat.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/model/Stat.java
@@ -10,11 +10,13 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "stats")
