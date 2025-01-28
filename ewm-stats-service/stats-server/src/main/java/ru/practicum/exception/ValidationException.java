eefbaf363d43b4b539e6b23575f06b034aefb8c6
eccentric58 <cool.eccentric@yandex.ru>
2025-01-28 11:29:08 +0300

    upd: 2-ая часть диплома

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/exception/ValidationException.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/exception/ValidationException.java
@@ -2,7 +2,7 @@ package ru.practicum.exception;
 
 public class ValidationException extends RuntimeException {
 
-    public ValidationException(String message) {
+    public ValidationException(final String message) {
         super(message);
     }
 }
